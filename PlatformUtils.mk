020be3d67dbff67534c5921e95c7df88049fbd42
Paul Schulze <pschulze@helindustries.com>
2025-05-04 21:28:29 +0200

    Add PlatformUtils.mk Makefile include

---
+++ PlatformUtils.mk
@@ -0,0 +1,44 @@
+PLATFORM_MAKEFILE_DIR := $(patsubst %/,%,$(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
+PLATFORM_UTILS_PRESENT := yes
+PYTHON ?= python
+MAKE_PLATFORM_UTILS ?= $(PYTHON) "$(PLATFORM_MAKEFILE_DIR)/make_platform_utils.py"
+PLATFORM := $(strip $(shell $(MAKE_PLATFORM_UTILS) --platform --print))
+PLATFORM_ID := $(strip $(shell $(MAKE_PLATFORM_UTILS) --platform --lower --print))
+PLATFORM_EXEC := $(strip $(shell $(MAKE_PLATFORM_UTILS) --platform-exec --print))
+PLATFORM_OPEN := $(strip $(shell $(MAKE_PLATFORM_UTILS) --platform-open --print))
+
+env-path = $(strip $(shell $(MAKE_PLATFORM_UTILS) $(1:%=--in "%") --env-path --print))
+platform-path = $(strip $(shell $(MAKE_PLATFORM_UTILS) $(1:%=--in "%") --platform-path --print))
+shell-list = $(strip $(shell $(MAKE_PLATFORM_UTILS) $(1:%=--in "%") --shell-list --print))
+lower = $(strip $(shell $(MAKE_PLATFORM_UTILS) --in $(1) --lower --print))
+upper = $(strip $(shell $(MAKE_PLATFORM_UTILS) --in $(1) --upper --print))
+exists = $(strip $(shell $(MAKE_PLATFORM_UTILS) --stoponerror false --in $(1) --exists --print))
+latest = $(strip $(shell $(MAKE_PLATFORM_UTILS) --stoponerror false --in $(1) --glob --sort asc --last --print))
+write = $(strip $(shell $(MAKE_PLATFORM_UTILS) --in $(1) --out $(2)))
+append = $(strip $(shell $(MAKE_PLATFORM_UTILS) --in $(1) --append $(2)))
+
+ifeq ($(strip $(PLATFORM_ID)),windows)
+    RM ?= del /Q /F
+    RMDIR ?= rmdir /S /Q
+    LS ?= dir /B
+    WHICH ?= where
+    COPY ?= copy /Y
+    MOVE ?= move /Y
+    TOUCH ?= $(MAKE_PLATFORM_UTILS) --touch
+    MKDIR ?= mkdir
+    FAIL ?= cmd /c exit 1
+    LN ?= $(MAKE_PLATFORM_UTILS) --symlink
+    SLEEP ?= timeout /T
+else
+    RM ?= rm -f
+    RMDIR ?= rm -rf
+    LS ?= ls -1 --color=never
+    WHICH ?= which
+    COPY ?= cp -f
+    MOVE ?= mv -f
+    TOUCH ?= touch
+    MKDIR ?= mkdir -p
+    FAIL ?= false
+    LN ?= ln -sf
+    SLEEP ?= sleep
+endif
